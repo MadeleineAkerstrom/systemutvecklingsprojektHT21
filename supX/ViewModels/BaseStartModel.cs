104629914b0bf5b465a5cff40a1ceffd47b7a8fd
antonsvedin <antonsvedin1@gmail.com>
2021-09-07 14:39:04 +0200

    testing bindings

--- supX/ViewModels/BaseStartModel.cs
+++ supX/ViewModels/BaseStartModel.cs
@@ -4,7 +4,15 @@ using System.Text;
 
 namespace supX.ViewModels
 {
-    public class BaseStartModel
+    public class BaseStartModel : BaseViewModel
     {
+        private string playerName = "anton";
+
+        public string PlayerName 
+        {
+            get { return playerName; }
+            set { playerName = value; }
+        }
+
     }
 }
