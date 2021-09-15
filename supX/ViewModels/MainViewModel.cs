4a7c0d9e439e051c0a55157514403ed69d864a4c
Madeleine Åkerström <akerstrommadde@gmail.com>
2021-09-15 10:52:51 +0200

    Cleaned viewmodels

--- supX/ViewModels/MainViewModel.cs
+++ supX/ViewModels/MainViewModel.cs
@@ -8,13 +8,9 @@ namespace supX.ViewModels
 {
     public class MainViewModel : BaseViewModel
     {
-        //public BaseViewModel CurrentViewModel { get; set; } = new ArenaViewModel();
-
-        //public BaseViewModel BalanceViewModel { get; set; } = new Player();
-
+     
         public CreatePlayerCommand createPlayerCommand = new CreatePlayerCommand();
-        //public string MyName { get; set; } = "Madde";
-        //public double MyBalance { get; set; 
+       
         public PlayerViewModel myPlayer { get; set; }
 
         private MainViewModel gameViewModel;
