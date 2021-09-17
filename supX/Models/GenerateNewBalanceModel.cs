394b01c7677f9eb4badce3ba101eebca2d40da47
Madeleine Åkerström <73692559+MadeleineAkerstrom@users.noreply.github.com>
2021-09-17 14:46:18 +0200

    devanton --> developMadde

--- supX/Models/GenerateNewBalanceModel.cs
+++ supX/Models/GenerateNewBalanceModel.cs
@@ -10,8 +10,29 @@ namespace supX.Models
         public double MyBalance { get; set; } 
         public double BetAmount { get; set; }
         public double Odds { get; set; }
-        public double NewBalance { get; set; }
+        public double NewBalance { get; set; } 
 
+      
+        public double CalculateNewBalance(GenerateResultsModel winner)
+        {            
+            double newBalance = NewBalance;        
+            double myBalance = MyBalance;
+            double betAmount = BetAmount;
+            double odds = Odds;
+            
+
+
+            if (winner = myBet)
+            {
+                newBalance = myBalance - betAmount;
+            }
+            else
+            {
+                newBalance = (betAmount * odds) + myBalance;
+            }
+
+            return newBalance;
+        }
        
 
         public GenerateNewBalanceModel()
