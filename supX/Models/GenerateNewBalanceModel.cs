db58c5334808e8023cbfb4090d74dc9d06f18d46
Madeleine Åkerström <73692559+MadeleineAkerstrom@users.noreply.github.com>
2021-09-17 13:41:07 +0200

    new GenerateNewBalanceModel class created

---
+++ supX/Models/GenerateNewBalanceModel.cs
@@ -0,0 +1,22 @@
+﻿using supX.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace supX.Models
+{
+    public class GenerateNewBalanceModel: BaseViewModel
+    {
+        public double MyBalance { get; set; } 
+        public double BetAmount { get; set; }
+        public double Odds { get; set; }
+        public double NewBalance { get; set; }
+
+       
+
+        public GenerateNewBalanceModel()
+        {
+
+        }
+    }
+}
