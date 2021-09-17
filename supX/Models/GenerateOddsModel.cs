9d9cff825dfbb88461ae8daeba209fd9081df7d5
antonsvedin <antonsvedin1@gmail.com>
2021-09-17 11:12:22 +0200

    added models for generating fights and odds

--- supX/Models/GenerateOddsModel.cs
+++ supX/Models/GenerateOddsModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using supX.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,22 @@ namespace supX.Models
 {
     public class GenerateOddsModel
     {
+        public void GenerateOdds(FighterViewModel fighter1, FighterViewModel fighter2)
+        {
+            int winChangePercentage = 50;
+
+            winChangePercentage = +(fighter1.Strength - fighter2.Defense) * 10;
+            winChangePercentage = +(fighter1.Cardio - fighter2.Cardio) * 10;
+            winChangePercentage = +(fighter1.Speed - fighter2.Defense) * 10;
+
+            double odds1 = 1 / (winChangePercentage / 100);
+            double odds2 = 1 / ((100 - winChangePercentage) / 100);
+
+        }
+
+        public GenerateOddsModel()
+        {
+
+        }
     }
 }
