665f4a5d6cc95c45f3012779542b670f8cf0ec28
Madeleine Åkerström <73692559+MadeleineAkerstrom@users.noreply.github.com>
2021-09-20 15:03:10 +0200

    IntegerToImageConverter

---
+++ supX/Converters/IntegerToImageConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace supX.Converters
+{
+    public class IntegerToImageConverter //: IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int result = (int)value;
+            switch (result)
+            {
+                case 0:
+                    return new Uri(@"\Assets\Images\Fighters\FighterLobster.png", UriKind.Relative);
+                case 1:
+                    return new Uri(@"\Assets\Images\Fighters\FighterKangaroo.png", UriKind.Relative);
+                case 2:
+                    return new Uri(@"\Assets\Images\Fighters\FighterTheHulk.png", UriKind.Relative);
+                case 3:
+                    return new Uri(@"\Assets\Images\Fighters\FighterHuman.png", UriKind.Relative);
+                case 4:
+                    return new Uri(@"\Assets\Images\Fighters\FighterFrog.png", UriKind.Relative);
+                case 5:
+                    return new Uri(@"\Assets\Images\Fighters\FighterSloth.png", UriKind.Relative);
+
+                default:
+                    break;
+            }
+            return null;
+        }
+    }
+}
