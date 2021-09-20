3f8af5ddd3b45a957c8e42fe10cac5e4be2db39e
Madeleine Åkerström <73692559+MadeleineAkerstrom@users.noreply.github.com>
2021-09-20 10:35:54 +0200

    Images and contentcontrol edited

--- supX/MainWindow.xaml.cs
+++ supX/MainWindow.xaml.cs
@@ -30,6 +30,7 @@ namespace supX
         {
             InitializeComponent();
             DataContext = new MainViewModel();
+            
             OpenFile();
 
             GenerateOddsModel generateOddsModel = new GenerateOddsModel();
