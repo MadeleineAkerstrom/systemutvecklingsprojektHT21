5adc910f7fac83f1dff9b4e9aa9c9a4dba3b416e
Isabelle Palmdahl <ispa2000@student.miun.se>
2021-09-24 10:16:46 +0100

    MainView and StartViewModel fixed

---
+++ supX/ViewModels/StartViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace supX.ViewModels
+{
+    public class StartViewModel : BaseViewModel
+    {
+        public MainViewModel Parent { get;}
+
+        public StartViewModel(MainViewModel mainViewModel)
+        {
+            Parent = mainViewModel;
+        }
+    }
+}
