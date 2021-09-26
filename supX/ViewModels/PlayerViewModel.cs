4661a4047ceed56f116139c0de4497d724c83513
JonasHagstrom79 <73818160+JonasHagstrom79@users.noreply.github.com>
2021-09-26 16:39:02 +0200

    Added Property to PlayerView

--- supX/ViewModels/PlayerViewModel.cs
+++ supX/ViewModels/PlayerViewModel.cs
@@ -6,20 +6,24 @@ namespace supX.ViewModels
 {
     public class PlayerViewModel : BaseViewModel
     {
-        public double MyBalance { get; set; } = 100;
+        //public double MyBalance { get; set; } = 100;
+        private double myBalance;
+        public double MyBalance
+        {
+            get { return myBalance; }
+            set { myBalance = value; }
+        }
         public MainViewModel Parent { get; }
 
         public PlayerViewModel(MainViewModel mainViewModel)
         {
             Parent = mainViewModel;
-        }
-        
-        
-        public PlayerViewModel()
-        {
-            
-        }
 
+            var myBalance = MyBalance;
+        }
+        
+        
+        
     }
 
    
