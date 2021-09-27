9e51a6b854786a50609d40ac02b43ec344efec9d
Madeleine Åkerström <73692559+MadeleineAkerstrom@users.noreply.github.com>
2021-09-27 10:48:13 +0200

    Backyardview

--- supX/ViewModels/BettingViewBackyardViewModel.cs
+++ supX/ViewModels/BettingViewBackyardViewModel.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
+using supX.Commands;
+using supX.Data;
 
 namespace supX.ViewModels
 {
@@ -11,16 +13,28 @@ namespace supX.ViewModels
         Sounds.SoundsAndMusic sounds = new Sounds.SoundsAndMusic();
         public MainViewModel Parent { get; }
         public GameViewModel GameVM { get; set; }
+        public FighterViewModel FighterVM { get; set; }
 
-        public int MyBet { get; set; }
-        public string BetAmount { get; set; } = "100";
+        public double BetAmount1 { get; set; }
+        public double BetAmount2 { get; set; }
+        public int MyBetId { get; set; } = 0;
+        public int FighterId1 { get; set; }
+        public int FighterId2 { get; set; }
+        public BettingViewBellagioViewModel betbell;
+
+
 
         public BettingViewBackyardViewModel(MainViewModel mainViewModel)
         {
             Parent = mainViewModel;
-            GameVM = new GameViewModel();                      
-            sounds.PlayBackyardIntroSound();
 
+            //sounds.PlayBellagioIntroSound();          
+
+            GameVM = new GameViewModel();
+            FighterVM = new FighterViewModel();
+            GameVM.GenerateArena();
+            FighterId1 = GameVM.Fighter1.Id;
+            FighterId2 = GameVM.Fighter2.Id;
         }
     }
 }
